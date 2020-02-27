70884931233530bb5b70bdc6cb2ea4ed7e0f5df8
xiattong <864380220@qq.com>
2020-02-27 14:42:20 +0800

    spring-basic 单例模式提交

---
+++ pattern/src/main/java/com/xiattong/pattern/singleton/register/ContainerSingleton.java
@@ -0,0 +1,31 @@
+package com.xiattong.pattern.singleton.register;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * 注册式单例
+ * Created by Tom.
+ */
+public class ContainerSingleton {
+
+    private ContainerSingleton(){}
+
+    private static Map<String,Object> ioc = new ConcurrentHashMap<String, Object>();
+
+    public static Object getInstance(String className){
+        Object instance = null;
+        if(!ioc.containsKey(className)){
+            try {
+                instance = Class.forName(className).newInstance();
+                ioc.put(className, instance);
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            return instance;
+        }else{
+            return ioc.get(className);
+        }
+    }
+
+}
---
+++ pattern/src/main/java/com/xiattong/pattern/singleton/test/ExectorThread.java
@@ -0,0 +1,19 @@
+package com.xiattong.pattern.singleton.test;
+
+import com.xiattong.pattern.singleton.threadlocal.ThreadLocalSingleton;
+
+/**
+ * Created by Tom.
+ */
+public class ExectorThread implements Runnable{
+
+    public void run() {
+
+//        LazySimpleSingletion instance = LazySimpleSingletion.getInstance();
+//        LazyDoubleCheckSingleton instance = LazyDoubleCheckSingleton.getInstance();
+        ThreadLocalSingleton instance = ThreadLocalSingleton.getInstance();
+        System.out.println(ThreadLocalSingleton.getInstance());
+        System.out.println(Thread.currentThread().getName() + ":" + instance);
+
+    }
+}
